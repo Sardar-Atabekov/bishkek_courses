1f183a492c6cbb1819ee69b9ca2809c1c52ffa5a
nurizzzka <nuriza.akynbaeva@mail.ru>
2019-02-01 00:53:36 +0300

    Adding router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navigation exact/>
+        <Navigation />
       </div>
     );
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './css/Navigation.css';
+import  Content from './Content';
 import 'antd/lib/menu/style/css';
 import { Menu, Icon } from 'antd';
 import { Router, Route, Switch } from 'react-router';
@@ -29,7 +30,13 @@ class Navigation extends Component {
 
     render() {
         let {titles} = this.state;
-
+        const Links = () => (
+            <main>
+                <Switch>
+                    <Route path='/content' component={Content}/>
+                </Switch>
+            </main>
+        );
         console.log(titles);
         return (
             <div style={{ width: 256 }} className="menu">
@@ -37,8 +44,8 @@ class Navigation extends Component {
                 <Menu mode="vertical" className="all-categories" style={{ width: 257,  background:6849}}>
                     {titles.map((one)=>{
                         return(
-                            <SubMenu className="titles" key={one.id} title={<span className="one-title">{one.title}</span>} >
-                                <MenuItemGroup style={{ width: 250 }} className="sub-categories">
+                            <SubMenu className="titles" key={one.id} title={<a href={one.title} className="one-title">{one.title}</a>} >
+                                <MenuItemGroup style={{ width: 250}} className="sub-categories">
                                     {one.subcategories.map((sub)=>{
                                         return(
                                             <Menu.Item className="sub-titles" key={sub.id}><span className="sub-title">{sub.title}</span></Menu.Item>
