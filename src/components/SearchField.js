496cd2746c14faa08e0f0b33b42dae2b1c0fab0b
nurizzzka <nuriza.akynbaeva@mail.ru>
2019-02-27 21:12:43 +0300

    Search

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,45 +1,40 @@
 import React, { Component } from 'react';
+import Universities from "./Universities";
+import {Link} from "react-router-dom";
 
  class SearchField extends Component {
      constructor(props) {
          super(props);
          this.state = {
              data: [],
+             courses: '',
          };
+         this.handleClick = this.handleClick.bind(this);
      }
+
      componentDidMount() {
-         const API = 'http://46.101.146.101:8081/courses/search/?search=';
+         const API = `http://46.101.146.101:8081/courses/search/?search=${this.state.courses}`;
          fetch(API)
              .then(response => response.json())
              .then(json => this.setState({data: json}))
      }
 
-     addItem() {
-
-         const newItem = document.getElementById("addInput")
-         const form = document.getElementById("addItemForm")
-     }
+     handleClick = (event) => {
+         this.setState({courses: event.target.value});
+     };
 
      render() {
-         const { data } = this.state.data;
-
          return (
              <div>
-                 <form className="form" id="addItemForm">
-                     <input
-                         type="text"
-                         className="input"
-                         id="addInput"
-                         placeholder="Поиск курсов..."
-                     />
-                     <button onClick={this.addItem}>
-                         Add Item
-                     </button>
+                 <form>
+                     <input placeholder="Поиск курса" />
+                     <button onClick={this.componentDidMount} value={this.state.courses}><Link to={'/searchingCourses'}>Поиск</Link></button>
                  </form>
-
+                 <div>
+                     {this.state.courses}
+                 </div>
              </div>
          )
      }
- }
-
+}
  export default SearchField;
