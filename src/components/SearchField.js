f733a4a30bf800b2b515d0a393e23bdf67663b7c
nurizzzka <nuriza.akynbaeva@mail.ru>
2019-03-01 11:21:29 +0300

    Printed only in console

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,37 +1,35 @@
 import React, { Component } from 'react';
-import Universities from "./Universities";
 import {Link} from "react-router-dom";
 
  class SearchField extends Component {
+
      constructor(props) {
          super(props);
          this.state = {
-             data: [],
-             courses: '',
+             courses: [
+             ]
          };
-         this.handleClick = this.handleClick.bind(this);
      }
 
-     componentDidMount() {
-         const API = `http://46.101.146.101:8081/courses/search/?search=${this.state.courses}`;
-         fetch(API)
-             .then(response => response.json())
-             .then(json => this.setState({data: json}))
-     }
+     gettingCourse = async (event) => {
+         event.preventDefault();
+         const course = event.target.elements.course.value;
+         const API_URL = await
+             fetch(`http://46.101.146.101:8081/courses/search/?search=${course}`);
+         const data = await API_URL.json();
+         console.log(data);
 
-     handleClick = (event) => {
-         this.setState({courses: event.target.value});
      };
 
      render() {
          return (
              <div>
-                 <form>
-                     <input placeholder="Поиск курса" />
-                     <button onClick={this.componentDidMount} value={this.state.courses}><Link to={'/searchingCourses'}>Поиск</Link></button>
+                 <form onSubmit={this.gettingCourse}>
+                     <input type="text" name="course" placeholder="City"/>
+                     <button>Get course</button>
                  </form>
                  <div>
-                     {this.state.courses}
+                     {this.courses}
                  </div>
              </div>
          )
