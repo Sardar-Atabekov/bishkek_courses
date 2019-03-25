1210d52df01c7814b8ae4c29fe9d559d40dc2b02
nurizzzka <nuriza.akynbaeva@mail.ru>
2019-03-26 03:37:10 +0600

    Added build

---
+++ build/precache-manifest.fc34238dbd4986e31a0b387321edf701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "56308278f036205ddaf19cfadfec94e6",
+    "url": "/static/media/neobis_logo.56308278.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d9f3fb6384f0bea29daa",
+    "url": "/static/js/main.d9f3fb63.chunk.js"
+  },
+  {
+    "revision": "c00c0ebf48c4ca510db8",
+    "url": "/static/js/1.c00c0ebf.chunk.js"
+  },
+  {
+    "revision": "d9f3fb6384f0bea29daa",
+    "url": "/static/css/main.d70cae32.chunk.css"
+  },
+  {
+    "revision": "c00c0ebf48c4ca510db8",
+    "url": "/static/css/1.b1f3827b.chunk.css"
+  },
+  {
+    "revision": "6a1e46d75ea2280739dc4bab89c1cbd0",
+    "url": "/index.html"
+  }
+];
